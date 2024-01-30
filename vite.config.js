e9656e194f5dfe65b2b282d5cd1620ae343c7887
Thevin Silva <thevinsilva@Thevins-MacBook-Air.local>
2024-01-30 00:51:47 +0000

    fixed config file

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // vite.config.js
-  build: {
-    rollupOptions: {
-      external: ['react-responsive'],
-      // ... other rollup options
-    },
-  },
 })
