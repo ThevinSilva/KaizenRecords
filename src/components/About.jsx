5b470805f38d2739b21a1c0519d6977ddf01dcce
Thevin Silva <thevin.silva@gmail.com>
2024-03-20 11:55:32 +0000

    fixed font size issue in about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,51 +1,38 @@
-// import TextStaggerAnimation from "../lib/TextStaggerAnimation";
 import styled from "styled-components";
 import TextStaggerAnimation from "../lib/TextStaggerAnimation";
 import { useMediaQuery } from "react-responsive";
-import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-const Centered = styled(motion.div)`
+const Container = styled(motion.div)`
   position: absolute;
   display: flex;
-  /* flex-direction: column; // This will stack children vertically
-  justify-content: center; // This will center children vertically in the container
-  align-items: center; // This will center children horizontally in the container */
+  flex-direction: column;
+  justify-content: start; // Align content to the top
+  align-items: start; // Align content to the left
   width: 100%;
-  height: 90%; // This will take the full height of the viewport
+  height: 90%;
   overflow: hidden;
-
   font-family: ${(props) => props.theme.fonts.primary};
   color: ${(props) => props.theme.colors.dark};
-  display: block;
   z-index: 100;
-  font-size: 5em;
 
-  @media (max-width: 1200px) {
-    font-size: 3em !important;
-  }
-
-  @media (max-width: 500px) {
-    font-size: 2em !important;
+  h2 {
+    font-size: clamp(2rem, 3vw + 2.3rem, 7rem);
+    margin: 0 0 20px 0; // Adjust margin to space out elements, remove if not needed
+    width: 100%;
+    text-align: left; // Align text to the left
+    word-wrap: break-word; // Prevents overflow by breaking long words
   }
 `;
 
 export default function About() {
-  const [isDesktop, setIsDesktop] = useState(false);
-  const desktop = useMediaQuery({ query: "(min-width : 768px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width : 768px)" });
 
-  useEffect(() => {
-    setIsDesktop(desktop);
-  }, [desktop]);
   return (
-    <Centered
-      //   initial={{
-      //     top: "0px",
-      //     left: "10px",
-      //   }}
+    <Container
       animate={{
-        top: `${isDesktop ? "50px" : "35px"}`,
-        left: `${isDesktop ? "50px" : "35px"}`,
+        top: isDesktop ? "50px" : "35px",
+        left: isDesktop ? "50px" : "35px",
         height: `calc(100% - ${isDesktop ? "100px" : "70px"})`,
         width: `calc(100% - ${isDesktop ? "100px" : "70px"})`,
         transition: { duration: 1 },
@@ -59,6 +46,6 @@ export default function About() {
         High-performance marketing. Full ownership rights for artists. No
         Bullsh*t.
       </TextStaggerAnimation>
-    </Centered>
+    </Container>
   );
 }
